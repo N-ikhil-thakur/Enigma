8155d7996d60c8e9ad12eb2a7583217e7f5694a1
unknown <nikhilthakur281@gmail.com>
2020-06-07 09:16:39 +0545

    initial

---
+++ rotate.py
@@ -0,0 +1,28 @@
+def rotate_dict_values(dic , step):
+    keys = list(dic.keys())
+    values = list(dic.values())
+    newValues = values.copy()
+
+    for i in range(0 , len(values)):
+        rot_step = abs(i+step) % len(values)
+        if((i+step) >= 0):
+            newValues[rot_step] = values[i]
+        else:
+            newValues[-rot_step] = values[i]
+
+    return  dict(zip(keys,newValues))
+
+
+def rotate_dict_keys(dic , step):
+    keys = list(dic.keys())
+    values = list(dic.values())
+    newKeys = keys.copy()
+
+    for i in range(0 , len(keys)):
+        rot_step = abs(i+step) % len(keys)
+        if((i+step) >= 0):
+            newKeys[rot_step] = keys[i]
+        else:
+            newKeys[-rot_step] = keys[i]
+
+    return  dict(zip(newKeys,values))
---
+++ test.py
@@ -0,0 +1,6 @@
+from rotate import rotate_dict_keys , rotate_dict_values
+
+age = {'Nikhil':18 , 'Niraj':14 , 'Nikita':21 , 'Mom':35}
+
+age = rotate_dict_keys(age , -1)
+print(age)
